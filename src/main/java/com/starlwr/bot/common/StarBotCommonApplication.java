446ce6a054493a95df00b745e9345eaba9291ffc
LWR <lwr1104@qq.com>
2025-03-09 21:59:17 +0800

    初始化仓库

---
+++ src/main/java/com/starlwr/bot/common/StarBotCommonApplication.java
@@ -0,0 +1,11 @@
+package com.starlwr.bot.common;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarBotCommonApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(StarBotCommonApplication.class, args);
+    }
+}
