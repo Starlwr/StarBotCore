75f52a927fc12b244928ec7fe7f3ef7e33596a5d
LWR <lwr1104@qq.com>
2025-04-02 02:45:14 +0800

    config: 增大 HTTP 请求缓冲区大小

--- src/main/java/com/starlwr/bot/common/config/WebClientConfig.java
+++ src/main/java/com/starlwr/bot/common/config/WebClientConfig.java
@@ -12,6 +12,7 @@ public class WebClientConfig {
     @Bean
     public WebClient webClient() {
         return WebClient.builder()
+                .codecs(configurer -> configurer.defaultCodecs().maxInMemorySize(8 * 1024 * 1024))
                 .build();
     }
 }
