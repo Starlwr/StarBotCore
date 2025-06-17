aa991a76813bfabcd064376fabb3de30de210ee6
LWR <lwr1104@qq.com>
2025-06-17 22:31:43 +0800

    config: 修改 WebClient 配置

--- src/main/java/com/starlwr/bot/common/config/WebClientConfig.java
+++ src/main/java/com/starlwr/bot/common/config/WebClientConfig.java
@@ -2,7 +2,9 @@ package com.starlwr.bot.common.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.client.HttpClient;
 
 /**
  * WebClient 配置
@@ -11,7 +13,10 @@ import org.springframework.web.reactive.function.client.WebClient;
 public class WebClientConfig {
     @Bean
     public WebClient webClient() {
+        HttpClient httpClient = HttpClient.create().followRedirect(true);
+
         return WebClient.builder()
+                .clientConnector(new ReactorClientHttpConnector(httpClient))
                 .codecs(configurer -> configurer.defaultCodecs().maxInMemorySize(16 * 1024 * 1024))
                 .build();
     }
