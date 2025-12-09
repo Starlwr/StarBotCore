0fc1954be2d50574a72605e20ee74fc02479d8ae
LWR <lwr1104@qq.com>
2025-12-09 21:00:58 +0800

    feat: 优化消息发送，支持设置消息发送前后回调

--- src/main/java/com/starlwr/bot/core/model/Message.java
+++ src/main/java/com/starlwr/bot/core/model/Message.java
@@ -11,6 +11,8 @@ import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.function.Predicate;
 
 /**
  * 消息，请使用 create 方法创建消息列表以自动处理 {next} 占位符
@@ -20,6 +22,11 @@ import java.util.Objects;
 @ToString
 @NoArgsConstructor
 public class Message {
+    /**
+     * 全局顺序号
+     */
+    private static final AtomicLong globalSequence = new AtomicLong(0);
+
     /**
      * 推送平台
      */
@@ -41,37 +48,64 @@ public class Message {
     private String content;
 
     /**
-     * StarBot 内部消息 ID，发送前自动由 StarBot 生成，无需手动设置
+     * StarBot 内部消息创建顺序号，通过 create 方法创建时自动生成，无需手动设置
      */
-    private Long id;
+    private Long sequence;
 
     /**
-     * 时间戳
+     * 创建时间戳，通过 create 方法创建时自动生成，无需手动设置
      */
-    private Instant timestamp;
+    private Instant createTime;
 
     /**
-     * 创建消息列表，自动处理 {next} 占位符
+     * 下一条消息，用于连接通过 {next} 占位符拆分的消息列表，通过 create 方法创建时自动生成，无需手动设置
+     */
+    private Message next;
+
+    /**
+     * 前一条消息，用于连接通过 {next} 占位符拆分的消息列表，通过 create 方法创建时自动生成，无需手动设置
+     */
+    private Message previous;
+
+    /**
+     * 消息 ID，消息发送成功后自动设置，无需手动设置
+     */
+    private String id;
+
+    /**
+     * 发送完毕时间戳，不论发送是否成功，消息发送后自动设置，无需手动设置
+     */
+    private Instant completeTime;
+
+    /**
+     * 发送前拦截回调列表，返回 false 会拦截消息发送，请勿调用阻塞操作
+     */
+    private List<Predicate<Message>> onBeforeSendInterceptors = new ArrayList<>();
+
+    /**
+     * 发送完毕回调列表，无论发送是否成功均会调用，请勿调用阻塞操作
+     */
+    private List<Runnable> onCompleteCallbacks = new ArrayList<>();
+
+    /**
+     * 发送成功回调列表，请勿调用阻塞操作
+     */
+    private List<Runnable> onSuccessCallbacks = new ArrayList<>();
+
+    /**
+     * 发送失败回调列表，请勿调用阻塞操作
+     */
+    private List<Runnable> onFailureCallbacks = new ArrayList<>();
+
+    /**
+     * 创建通过 next 字段和 previous 字段相连接的消息列表，自动处理 {next} 占位符
      * @param platform 推送平台
      * @param type 推送目标类型
      * @param num 账号或群号，根据推送目标类型而定
      * @param content 可包含占位符的消息内容
-     * @return 消息列表
+     * @return 通过 next 字段和 previous 字段相连接的消息列表
      */
     public static List<Message> create(String platform, PushTargetType type, Long num, String content) {
-        return create(platform, type, num, content, Instant.now());
-    }
-
-    /**
-     * 创建消息列表，自动处理 {next} 占位符
-     * @param platform 推送平台
-     * @param type 推送目标类型
-     * @param num 账号或群号，根据推送目标类型而定
-     * @param content 可包含占位符的消息内容
-     * @param timestamp 时间戳
-     * @return 消息列表
-     */
-    public static List<Message> create(String platform, PushTargetType type, Long num, String content, Instant timestamp) {
         List<Message> messages = new ArrayList<>();
 
         String[] parts = content.split("\\{next}");
@@ -85,8 +119,18 @@ public class Message {
             message.setType(type);
             message.setNum(num);
             message.setContent(part);
-            message.setTimestamp(timestamp);
+            message.setSequence(globalSequence.incrementAndGet());
+            message.setCreateTime(Instant.now());
             messages.add(message);
+        }
+
+        for (int i = 0; i < messages.size(); i++) {
+            if (i > 0) {
+                messages.get(i).setPrevious(messages.get(i - 1));
+            }
+            if (i < messages.size() - 1) {
+                messages.get(i).setNext(messages.get(i + 1));
+            }
         }
 
         return messages;
@@ -118,5 +162,37 @@ public class Message {
                 .replaceAll("\\{image_url=.*?}", "[图片]")
                 .replaceAll("\\{image_path=.*?}", "[图片]")
                 .replaceAll("\\{image_base64=.*?}", "[图片]");
+    }
+
+    /**
+     * 添加发送前拦截回调，返回 false 会拦截消息发送
+     * @param interceptor 发送前拦截回调，返回 false 会拦截消息发送
+     */
+    public void addOnBeforeSendInterceptor(Predicate<Message> interceptor) {
+        this.onBeforeSendInterceptors.add(interceptor);
+    }
+
+    /**
+     * 添加发送完毕回调，无论发送是否成功均会调用
+     * @param callback 发送完毕回调，无论发送是否成功均会调用
+     */
+    public void addOnCompleteCallback(Runnable callback) {
+        this.onCompleteCallbacks.add(callback);
+    }
+
+    /**
+     * 添加发送成功回调
+     * @param callback 发送成功回调
+     */
+    public void addOnSuccessCallback(Runnable callback) {
+        this.onSuccessCallbacks.add(callback);
+    }
+
+    /**
+     * 添加发送失败回调
+     * @param callback 发送失败回调
+     */
+    public void addOnFailureCallback(Runnable callback) {
+        this.onFailureCallbacks.add(callback);
     }
 }
--- src/main/java/com/starlwr/bot/core/sender/StarBotMessageSender.java
+++ src/main/java/com/starlwr/bot/core/sender/StarBotMessageSender.java
@@ -11,18 +11,20 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.Instant;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Optional;
 import java.util.concurrent.*;
+import java.util.function.Predicate;
 
 /**
- * StarBot 推送消息发送器
+ * StarBot 消息发送器
  */
 @Slf4j
 @Service
-public class StarBotPushMessageSender {
+public class StarBotMessageSender {
     private final HttpUtil http;
 
     private final StarBotSenderService senderService;
@@ -34,7 +36,7 @@ public class StarBotPushMessageSender {
     private final Map<String, Future<?>> platformTasks = new ConcurrentHashMap<>();
 
     @Autowired
-    public StarBotPushMessageSender(HttpUtil http, StarBotSenderService senderService) {
+    public StarBotMessageSender(HttpUtil http, StarBotSenderService senderService) {
         this.http = http;
         this.senderService = senderService;
     }
@@ -74,11 +76,9 @@ public class StarBotPushMessageSender {
             Thread.currentThread().setName("sender-" + sender.getName());
             log.info("{} 平台消息发送线程已启动", sender.getName());
             long delay = sender.getDelay();
-            long id = 1;
             while (!Thread.currentThread().isInterrupted()) {
                 try {
                     Message message = queue.take();
-                    message.setId(id++);
                     doSend(sender, message);
                     Thread.sleep(delay);
                 } catch (InterruptedException e) {
@@ -98,8 +98,6 @@ public class StarBotPushMessageSender {
      * @param message 消息
      */
     private void doSend(Sender sender, Message message) {
-        String platform = sender.getName();
-
         Map<String, String> headers = new HashMap<>();
         if (StringUtil.isNotBlank(sender.getToken())) {
             headers.put("Authorization", "Bearer " + sender.getToken());
@@ -110,13 +108,48 @@ public class StarBotPushMessageSender {
         params.put("type", message.getType().getCode());
         params.put("num", message.getNum());
         params.put("content", message.getContent());
-        params.put("timestamp", message.getTimestamp().toEpochMilli());
+        params.put("sequence", message.getSequence());
+        params.put("create_time", message.getCreateTime().toEpochMilli());
+
+        for (Predicate<Message> interceptor : message.getOnBeforeSendInterceptors()) {
+            if (!interceptor.test(message)) {
+                log.info("已取消发送消息: StarBot -> {} ([{}] {}) [{}]: {}", sender.getName(), message.getType().getStr(), message.getNum(), message.getSequence(), message.getDisplay());
+                return;
+            }
+        }
 
         JSONObject result = http.postJson(sender.getUrl(), headers, params);
+        message.setCompleteTime(Instant.now());
+
+        for (Runnable callback : message.getOnCompleteCallbacks()) {
+            try {
+                callback.run();
+            } catch (Exception e) {
+                log.error("执行消息发送完毕回调异常: [{}]{}", message.getSequence(), message.getDisplay(), e);
+            }
+        }
+
         if (result.getInteger("code") == 0) {
-            log.info("StarBot -> {} ([{}] {}) [{}]: {}", platform, message.getType().getStr(), message.getNum(), message.getId(), message.getDisplay());
+            message.setId(result.getString("id"));
+            log.info("StarBot -> {} ([{}] {}) [{}]: {}", sender.getName(), message.getType().getStr(), message.getNum(), message.getSequence(), message.getDisplay());
+
+            for (Runnable callback : message.getOnSuccessCallbacks()) {
+                try {
+                    callback.run();
+                } catch (Exception e) {
+                    log.error("执行消息发送成功回调异常: [{}]{}", message.getSequence(), message.getDisplay(), e);
+                }
+            }
         } else {
-            log.error("消息发送失败 ({}): StarBot -> {} ([{}] {}) [{}]: {}", result.getString("message"), platform, message.getType().getStr(), message.getNum(), message.getId(), message.getDisplay());
+            log.error("消息发送失败 ({}): StarBot -> {} ([{}] {}) [{}]: {}", result.getString("message"), sender.getName(), message.getType().getStr(), message.getNum(), message.getSequence(), message.getDisplay());
+
+            for (Runnable callback : message.getOnFailureCallbacks()) {
+                try {
+                    callback.run();
+                } catch (Exception e) {
+                    log.error("执行消息发送失败回调异常: [{}]{}", message.getSequence(), message.getDisplay(), e);
+                }
+            }
         }
     }
 }
